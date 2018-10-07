bedabd1896ef46ec1551fe79c6563d6bc8f8764f
Sorin <sorin.biriescu@gmail.com>
2018-10-07 21:23:43 +0200

    Added schema for list of POI

--- app/api/resources.py
+++ app/api/resources.py
@@ -6,7 +6,7 @@ from app.api.status import HTTP_404_NOT_FOUND, HTTP_200_OK
 from sqlalchemy import func
 from shapely import wkb
 from shapely.geometry import Point, mapping
-from marshmallow import post_dump
+from marshmallow import post_dump, pre_dump
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
@@ -20,28 +20,40 @@ class POISchema(ma.Schema):
 
     def get_lat_long(self, obj):
         point_wkb = wkb.loads(bytes(obj.centroid.data))
-        lat_long = mapping(Point(point_wkb.x, point_wkb.y))
+        lat_long = mapping(Point(point_wkb.y, point_wkb.x))
         return lat_long
 
 
     @post_dump(pass_many=False)
     def wrap(self, data):
-        return {
-            "type": "FeatureCollection",
-            "features": [{    
-                "type": "Feature",
-                "properties": {
-                    "id" : data["id"],
-                    "osm_id": data["osm_id"],
-                    "osm_type": data["osm_type"],
-                    "name": data["name"],
-                    "description": data["description"],
-                    "attraction_type": data["attraction_type"]
-                },
-                "geometry": data["lat_long"]
-            }]
+        return { 
+            "type": "Feature",
+            "properties": {
+                "id" : data["id"],
+                "osm_id": data["osm_id"],
+                "osm_type": data["osm_type"],
+                "name": data["name"],
+                "description": data["description"],
+                "attraction_type": data["attraction_type"]
+            },
+            "geometry": data["lat_long"]
         }
 
+class POIListSchema(ma.Schema):
+    results = []
+
+    @pre_dump(pass_many=True)
+    def serialize_poi(self, data, many):
+        for poi in data:
+            serialized_poi = POISchema().dump(poi).data
+            self.results.append(serialized_poi)
+
+    @post_dump(pass_many=False)
+    def wrap(self, data):
+        return {
+            "type": "FeatureCollection",
+            "features": [poi for poi in self.results]
+        }
     
 def get_POI_or_404(id):
     poi = Attractions.query \
@@ -65,15 +77,13 @@ class POIList(Resource):
             "features" : [poi for poi in query]
         }
         
-        print(poi_list)
-        return poi_list, HTTP_200_OK
+        result = POIListSchema().dump(query).data
+        return result, HTTP_200_OK
 
 class POIItem(Resource):
-    # @marshal_with(result_item)
     def get(self, id):
         poi = get_POI_or_404(id)
         result = POISchema().dump(poi).data
-        print(result)
         return result, HTTP_200_OK
 
 
