77b876df0228581851ed2bcdb6a3c7db428999e7
sorinbiriescu <31899619+sorinbiriescu@users.noreply.github.com>
2018-10-14 14:25:04 +0200

    Delete run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-import os
-from app.main import create_app
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app = create_app(config_filename="development.cfg")
-    app.run(host="0.0.0.0", port=port)
